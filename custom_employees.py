e5336beccdd29f8f59754fbb9c35bcff7ee3f9e9
Miroslav <miroslav.savel@gmail.com>
2022-05-22 20:27:25 +0200

    1.task - add a new boolean field on the Employee screen after the work_email

--- custom_employees.py
+++ custom_employees.py
@@ -2,9 +2,11 @@
 
 from odoo import models, fields
 class custom_info(models.Model):
-    _inherit = 'hr.employee'
+    _inherit = 'hr.employee'        # inherit from existing model https://www.youtube.com/watch?v=z1Tx7EGkPy0&list=PLqRRLx0cl0hoJhjFWkFYowveq2Zn55dhM&index=12
     #NISS = fields.Char(string="NISS : ", required=True)
     #NISS = fields.Boolean(default=True, required=True)
     #NISS = fields.Boolean(string="I love gymbeam")
-    NISS = fields.Char(string="NISS : ", required=True)
+    # NISS = fields.Char(string="NISS : ", required=True)
+    # nove_pole = fields.Char(string="nove_pole : ", required=True)
+    i_love_gb= fields.Boolean(default=True, required=True)
 
